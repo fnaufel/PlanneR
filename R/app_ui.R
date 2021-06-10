f5a7c781670cfaa17cf8a7a5cb123d9a7699a325
Fernando Naufel <fnaufel@gmail.com>
2021-06-10 14:09:11 -0300

    Change favicon

--- R/app_ui.R
+++ R/app_ui.R
@@ -163,7 +163,7 @@ golem_add_external_resources <- function(){
   )
  
   tags$head(
-    golem::favicon(),
+    golem::favicon(ext = 'png'),
     golem::bundle_resources(
       path = app_sys('app/www'),
       app_title = 'PlanneR'
