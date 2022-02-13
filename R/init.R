c22bfe673a5c4ce7b432e6624fbbb5fc82c7f0b4
Fernando Naufel <fnaufel@gmail.com>
2022-02-13 14:57:01 -0300

    Update for 2022.1

--- R/init.R
+++ R/init.R
@@ -2,9 +2,9 @@
 # Initialization ----------------------------------------------------------
 
 chosen_theme <- 'journal'
-version_no <- '2.0'
-default_start <- lubridate::dmy('14/06/2021')
-default_end <- lubridate::dmy('25/09/2021')
+version_no <- '2.1'
+default_start <- lubridate::dmy('28/03/2022')
+default_end <- lubridate::dmy('30/07/2022')
 default_days <- c(3, 5)
 
 wday_names <- c(
